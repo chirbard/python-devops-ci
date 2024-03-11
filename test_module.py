26dbd9bcc988acb78e7a25a614c8ebe7cb0f3aba
chirbard <matamm@outlook.com>
2024-03-11 08:59:56 +0200

    new test

--- test_module.py
+++ test_module.py
@@ -2,11 +2,15 @@ from src.main import add
 
 
 class TestClass:
-    def test_add_int(self):
-        assert add (3, 5) == 8  
+    def test_add_int_1(self):
+        assert add (3, 5) == 8
+
+    def test_add_int_2(self):
+        assert add (3, -5) == -2
 
     def test_add_float(self):
-        assert add (3.5, 5.3) == 8.8 
+        assert add (3.5, 5.3) == 8.8
 
     def test_add_string(self):
         assert add ('Hello ', 'World') == 'Hello World'
+
