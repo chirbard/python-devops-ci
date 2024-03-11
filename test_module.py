3e33db4ce008706489c4f93c1540fd2ba8e70763
chirbard <matamm@outlook.com>
2024-03-11 09:37:12 +0200

    test change

--- test_module.py
+++ test_module.py
@@ -6,7 +6,7 @@ class TestClass:
         assert add (3, 5) == 8
 
     def test_add_int_2(self):
-        assert add (3, -5) == -2
+        assert add (3, -6) == -3
 
     def test_add_float(self):
         assert add (3.5, 5.3) == 8.8
